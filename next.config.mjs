38fc56c0520dda67fec0334f839737e1122f6364
M-Abdullah-Akbar <muhammedabdullahakbar@gmail.com>
2025-08-22 19:37:46 +0500

    updated api link using nextsol

--- next.config.mjs
+++ next.config.mjs
@@ -50,9 +50,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
+        protocol: "https",
+        hostname: "spn.nexteksol.com",
+        port: "",
         pathname: "/**",
       },
     ],
